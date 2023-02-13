1d677a4c89a6ecf539352329336f2f469bfef7de
Vladimir Matkovskii <vlavlamat@icloud.com>
2023-02-13 09:25:36 -0400

    mod: .eslintrc.yml, Makefile, cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,7 @@ import readlineSync from 'readline-sync';
 
 function askAndAnswer() {
   const userName = readlineSync.question('May I have your name? ');
-  // console.log(`Hello, ${userName}!`);
-  console.log('Hello, ' + userName + '!');
+  console.log(`Hello, ${userName}!`);
 }
 
 export default askAndAnswer;
