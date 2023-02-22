59569849629651b60e199242be29495b2e55dd10
Vladimir Matkovskii <vlavlamat@icloud.com>
2023-02-22 13:41:27 -0400

    function gameCycle

--- games/even.js
+++ games/even.js
@@ -4,6 +4,7 @@ import {
   getRandomNumber,
   evenOrNot,
   questionAndAnswer,
+  gameCycle,
 } from '../src/index.js';
 
 // Greetings and getting username:
@@ -14,23 +15,15 @@ console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
 // Let's play:
 function letsPlay() {
-  const minRangeNumber = 1; // Set up a range for random number
-  const maxRangeNumber = 30;
-  const randomNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get random number
-  const trueAnswer = evenOrNot(randomNumber); // Get true answer
-  const answer = questionAndAnswer(randomNumber); // Asking question and getting answer
-  if (answer === 'yes' || answer === 'no') {
-    if (answer === trueAnswer) {
-      let count = 0;
-      console.log('Correct!');
-      while (count < 2) {
-        count += 1;
-        letsPlay();
-      }
-    }
-  } else if (answer !== 'yes' || answer !== 'no') {
-    console.log('Please, use "yes" or "no" in your answer');
-    letsPlay();
+  const result = gameCycle();
+  switch (result) {
+    case 1:
+      gameCycle();
+      break;
+    case 2:
+      break;
+    default:
+      gameCycle();
   }
 }
 
