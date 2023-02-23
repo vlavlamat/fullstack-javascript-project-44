c117d736261fde6b80e4d13e188994491f36ec4b
Vladimir Matkovskii <vlavlamat@icloud.com>
2023-02-23 11:07:55 -0400

    mod: even.js and index.js

--- games/even.js
+++ games/even.js
@@ -4,26 +4,81 @@ import {
   getRandomNumber,
   evenOrNot,
   questionAndAnswer,
-  gameCycle,
 } from '../src/index.js';
 
 // Greetings and getting username:
-greeting();
+const gamerName = greeting();
 
 // Declare game rules:
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
+// Game cycle:
+function gameCycle() {
+  let result;
+  const minRangeNumber = 1; // Set up a range for random number
+  const maxRangeNumber = 30;
+  const randomNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get random number
+  const trueAnswer = evenOrNot(randomNumber); // Get true answer
+  const answer = questionAndAnswer(randomNumber); // Asking question and getting answer
+  if (answer === 'yes' || answer === 'no') {
+    if (answer === trueAnswer) {
+      console.log('Correct!');
+      result = true;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'`);
+      result = false;
+    }
+  } else if (answer !== 'yes' || answer !== 'no') {
+    console.log('Please, use "yes" or "no" in your answer');
+  }
+  return result;
+}
+
 // Let's play:
+// function letsPlay() {
+//   let result = gameCycle();
+//   switch (result) {
+//     case true:
+//       result = gameCycle();
+//       switch (result) {
+//         case true:
+//           result = gameCycle();
+//           switch (result) {
+//             case true:
+//               console.log(`Congratulations, ${gamerName}!`);
+//               break;
+//             case false:
+//               console.log(`Let's try again, ${gamerName}!`);
+//               break;
+//             default:
+//               letsPlay();
+//           }
+//           break;
+//         case false:
+//           console.log(`Let's try again, ${gamerName}!`);
+//           break;
+//         default:
+//           letsPlay();
+//       }
+//       break;
+//     case false:
+//       console.log(`Let's try again, ${gamerName}!`);
+//       break;
+//     default:
+//       letsPlay();
+//   }
+// }
+
 function letsPlay() {
-  const result = gameCycle();
-  switch (result) {
-    case 1:
-      gameCycle();
-      break;
-    case 2:
-      break;
-    default:
-      gameCycle();
+  let result = gameCycle();
+  if (result === true) {
+    for (let i = 0; i < 2; i += 1) {
+      result = gameCycle();
+    }
+    console.log(`Congratulations, ${gamerName}!`);
+  }
+  if (result === false) {
+    console.log(`Let's try again, ${gamerName}!`);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -43,34 +43,10 @@ function getOperatorSymbol() {
   }
 }
 
-function gameCycle() {
-  let result;
-  const minRangeNumber = 1; // Set up a range for random number
-  const maxRangeNumber = 30;
-  const randomNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get random number
-  const trueAnswer = evenOrNot(randomNumber); // Get true answer
-  const answer = questionAndAnswer(randomNumber); // Asking question and getting answer
-  if (answer === 'yes' || answer === 'no') {
-    if (answer === trueAnswer) {
-      console.log('Correct!');
-      result = 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'`);
-      result = 2;
-    }
-  } else if (answer !== 'yes' || answer !== 'no') {
-    console.log('Please, use "yes" or "no" in your answer');
-    result = 3;
-    gameCycle();
-  }
-  return result;
-}
-
 export {
   getRandomNumber,
   greeting,
   evenOrNot,
   getOperatorSymbol,
   questionAndAnswer,
-  gameCycle,
 };
