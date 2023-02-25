53e273934a11087bbff54b440b7e941f06f78255
Vladimir Matkovskii <vlavlamat@icloud.com>
2023-02-25 11:17:40 -0400

    mod: calc.js, even.js

--- games/calc.js
+++ games/calc.js
@@ -11,10 +11,9 @@ const rule = 'What is the result of the expression?';
 const gamerName = greetingAndRules(rule);
 
 function letsCalculate() {
-  let count = 0;
   const minRangeNumber = 1; // Set up a range for random numbers
   const maxRangeNumber = 50;
-  while (count < 3) {
+  for (let i = 0; i < 3; i += 1) {
     const firstNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get first random number
     const secondNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get second random
     const operator = getOperatorSymbol(); // Get random operator
@@ -22,7 +21,6 @@ function letsCalculate() {
     const answer = questionAndAnswer(firstNumber, secondNumber, operator); // Get an answer
     if (answer === correctResult) {
       console.log('Correct!');
-      count += 1;
     }
     if (answer !== correctResult) {
       return `'${answer}' is wrong answer ;(. Correct answer was '${correctResult}'.\nLet's try again, ${gamerName}!`;
--- games/even.js
+++ games/even.js
@@ -10,16 +10,14 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no"';
 const gamerName = greetingAndRules(rule);
 
 function guessEven() {
-  let count = 0;
   const minRangeNumber = 1; // Set up a range for random numbers
   const maxRangeNumber = 30;
-  while (count < 3) {
+  for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomNumber(minRangeNumber, maxRangeNumber); // Get random number
     const trueAnswer = evenOrNot(randomNumber); // Get true answer
     const answer = questionAndAnswer(randomNumber); // Asking question and getting answer
     if (answer === trueAnswer) {
       console.log('Correct!');
-      count += 1;
     }
     if (answer !== trueAnswer) {
       return `'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${gamerName}!`;
